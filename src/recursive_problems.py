0f4f48081856a73b5c75a9843175a88d2f0d24d3
Jimmy Jose <jimi.jose@gmail.com>
2021-05-20 15:09:35 -0700

    Add algorithm to determine maxinimum element in an array recursively

--- src/recursive_problems.py
+++ src/recursive_problems.py
@@ -1,11 +1,21 @@
-def sumList(list):
+def sum_list(list):
     if list == []:
         return 0
     
-    return list[0] + sumList(list[1:])
+    return list[0] + sum_list(list[1:])
 
-def sizeList(list):
+def size_list(list):
     if list == []:
         return 0
     
-    return 1 + sizeList(list[1:])    
+    return 1 + size_list(list[1:])
+
+def max_list(list):
+    if list is None:
+        raise Exception("Empty list cannot be processed!")
+
+    if len(list) == 1:
+        return list[0]
+    
+    sub_max = max_list(list[1:])
+    return list[0] if list[0] > sub_max else sub_max
--- tests/recursive_problems_test.py
+++ tests/recursive_problems_test.py
@@ -2,13 +2,23 @@ import pytest
 from src.recursive_problems import *
 
 def test_given_empty_list_then_sum_is_0():
-    assert sumList([]) == 0
+    assert sum_list([]) == 0
 
 def test_given_valid_list_then_sum_is_returned():
-    assert sumList([2, 4, 6]) == 12
+    assert sum_list([2, 4, 6]) == 12
 
 def test_given_empty_list_then_size_is_0():
-    assert sizeList([]) == 0
+    assert size_list([]) == 0
 
 def test_given_valid_list_then_size_is_returned():
-    assert sizeList([2, 4, 6]) == 3
+    assert size_list([2, 4, 6]) == 3
+
+def test_given_empty_list_then_exception_raised():
+    with pytest.raises(Exception):
+        max_list([])
+
+def test_given_single_value_in_list_then_max_is_itself():
+    assert max_list([10]) == 10
+
+def test_given_valid_list_then_max_is_determined():
+    assert max_list([10, 88, 55, 220, 11]) == 220
